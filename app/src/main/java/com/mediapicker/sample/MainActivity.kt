03b55c803d6239f67a37ea39cd1c38d8a46aada2
Amit Pathak <amit.pathak@olx.com>
2020-04-27 00:56:03 +0530

    test commit

--- app/src/main/java/com/mediapicker/sample/MainActivity.kt
+++ app/src/main/java/com/mediapicker/sample/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : AppCompatActivity() {
 
         Gallery.init(galleryConfig)
         try {
+
             val transaction = supportFragmentManager.beginTransaction()
             fragment = HomeFragment.getInstance(SelectedItemHolder.listOfSelectedPhotos,
                 SelectedItemHolder.listOfSelectedVideos,
